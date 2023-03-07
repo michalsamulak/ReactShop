651dbc441fc52245a3eb77ba5745f1c53722b22a
Michal Samulak <sanchossjmistik@gmail.com>
2023-03-07 17:43:28 +0100

    style Cart footer

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,15 +1,40 @@
-import React from 'react'
-import { CartContainer, CartFooterBox, CartFooterWrapper, CartProduktWrapper, DiscountInput } from '../style/Cart.style'
+import {
+    CartCheckoutBtn,
+    CartCheckoutPaymants,
+    CartContainer,
+    CartFooterBox,
+    CartFooterPrice,
+    CartFooterTitle,
+    CartFooterWrapper,
+    CartProduktWrapper,
+    CartTitleBox,
+    DiscountInput,
+    PaymantsLogo,
+} from "../style/Cart.style";
 
 export const Cart = () => {
-  return (
-    <CartContainer>
-        <CartProduktWrapper>text</CartProduktWrapper>
-        <CartFooterWrapper>
-        <CartFooterBox><DiscountInput placeholder='Promo code'/></CartFooterBox>
-        <CartFooterBox>s</CartFooterBox>
+    return (
+        <CartContainer>
+            <CartProduktWrapper>text</CartProduktWrapper>
+            <CartFooterWrapper>
+                <CartFooterBox>
+                    <DiscountInput placeholder="Promo code" />
+                </CartFooterBox>
+                <CartFooterBox>
+                    <CartTitleBox>
+                        <CartFooterTitle>Subtotal</CartFooterTitle>
+                        <CartFooterPrice>$55.55</CartFooterPrice>
+                    </CartTitleBox>
 
-        </CartFooterWrapper>
-    </CartContainer>
-  )
-}
+                    <CartCheckoutBtn>Checkout</CartCheckoutBtn>
+                    <CartCheckoutPaymants>
+                        <PaymantsLogo src="https://cdn-icons-png.flaticon.com/512/196/196578.png" />
+                        <PaymantsLogo src="https://img.icons8.com/color/256/mastercard-logo.png" />
+
+                        <PaymantsLogo src="https://cdn-icons-png.flaticon.com/512/196/196565.png" />
+                    </CartCheckoutPaymants>
+                </CartFooterBox>
+            </CartFooterWrapper>
+        </CartContainer>
+    );
+};
--- src/style/Cart.style.ts
+++ src/style/Cart.style.ts
@@ -17,9 +17,7 @@ export const CartProduktWrapper = styled.div`
 
 export const CartFooterWrapper = styled.div`
     height: 150px;
-//test color
 
-    background-color: aquamarine;
     display: flex;
     justify-content: space-between;
 
@@ -27,32 +25,75 @@ export const CartFooterWrapper = styled.div`
 
 export const CartFooterBox = styled.div`
     margin: 10px;
-//test color
-    background-color: beige;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+
 `
 
 export const DiscountInput = styled.input`
     font-weight: lighter;
     font-size: 14px;
     text-align: center;
-    letter-spacing: 3px;
+    letter-spacing: 2px;
     background: none;
 
     padding: 10px;
-    border: 1px solid grey;
+    border: 1px solid #80808045;
 `
+export const CartTitleBox = styled.div`
+    display:flex;
+    justify-content: space-between;
 
+    font-weight: bold;
+    font-size: 16px;
+    margin: 10px;
+    width: 200px;
+`
 export const CartFooterTitle = styled.div`
 
 `
+
+export const CartFooterPrice = styled.div`
+
+`
+
 export const CartFooterDisclaimer = styled.div`
 
 `
 
 export const CartCheckoutBtn = styled.button`
 
+width: 150px;
+display: flex;
+flex-direction: column;
+align-items: center;
+padding: 6px 14px;
+font-family: -apple-system, BlinkMacSystemFont, 'Roboto', sans-serif;
+border-radius: 6px;
+border: none;
+
+color: #fff;
+background: linear-gradient(180deg, #4B91F7 0%, #367AF6 100%);
+ background-origin: border-box;
+box-shadow: 0px 0.5px 1.5px rgba(54, 122, 246, 0.25), inset 0px 0.8px 0px -0.25px rgba(255, 255, 255, 0.2);
+user-select: none;
+-webkit-user-select: none;
+touch-action: manipulation;
+
+&:focus {
+box-shadow: inset 0px 0.8px 0px -0.25px rgba(255, 255, 255, 0.2), 0px 0.5px 1.5px rgba(54, 122, 246, 0.25), 0px 0px 0px 3.5px rgba(58, 108, 217, 0.5);
+outline: 0;
+}
+
 `
 
 export const CartCheckoutPaymants = styled.div`
 
+`
+
+export const PaymantsLogo = styled.img`
+    width: 50px;
+    padding: 5px;
 `
